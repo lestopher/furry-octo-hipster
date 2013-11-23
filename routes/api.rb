181fe73a4dd6df4d2e6cd4aa858ef11d853a8fa8
christopher nguyen <christoph.nguyen@gmail.com>
2013-11-23 15:46:05 -0800

    Updating to use angular better

--- routes/api.rb
+++ routes/api.rb
@@ -5,10 +5,19 @@ class Instasearch < Sinatra::Application
 
   namespace '/api' do
     namespace '/user' do
-      get '/feed' do
+      get '/feed/?' do
         client = Instagram.client(access_token: session[:access_token])
         user = client.user
-        recent_media = client.user_recent_media
+        p = Hash.new
+        p[:max_id] = params[:id] if params[:id]
+
+        recent_media = client.user_recent_media p
+
+        if recent_media.pagination.next_max_id
+          recent_media["next_api_params"] = {
+            id: recent_media.pagination.next_max_id
+          }
+        end
 
         if recent_media
           status 200
@@ -19,6 +28,33 @@ class Instasearch < Sinatra::Application
             message: "Something got jacked up."
           }.to_json
         end
+      end
+    end
+
+    get '/tag/:tag/?' do
+      client              = Instagram.client(access_token: session[:access_token])
+      p                   = Hash.new
+      p[:min_tag_id]      = params[:id] if params[:position] == 'after'
+      p[:max_tag_id]      = params[:id] if params[:position] == 'before'
+      puts p
+      media               = client.tag_recent_media params[:tag], p
+
+      if media.pagination.next_max_tag_id
+        media["next_api_params"] = {
+          tag: params[:tag],
+          position: 'before',
+          id: media.pagination.next_max_tag_id
+        }
+      end
+
+      if media
+        status 200
+        media.to_json
+      else
+        status 500
+        {
+          message: "Tag failed?"
+        }.to_json
       end
     end
   end
--- routes/user.rb
+++ routes/user.rb
@@ -1,9 +1,17 @@
 class Instasearch < Sinatra::Application
-  namespace '/user' do
+  namespace '' do
     get '/feed' do
       redirect '/' unless session[:access_token]
 
-      slim :feed
+      slim :feed, :locals => { ngController: 'InstasearchFeedListController' }
+    end
+
+    get '/map' do
+      slim :map, :locals => { :api => nil, :params => nil, :type => "map" }
+    end
+
+    get '/tag/:tag' do
+      slim :feed, :locals => { ngController: 'TagListController' }
     end
   end
 end
